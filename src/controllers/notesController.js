e2d68686829449ea4a7fc562a3e1d5d0ab74f242
Anastasiia <gorgona1597531597@gmail.com>
2025-11-23 18:46:59 +0100

    create auth

--- src/controllers/notesController.js
+++ src/controllers/notesController.js
@@ -1,77 +1,17 @@
-import { Note } from '../models/note.js';
-import createHttpError from 'http-errors';
+import { Router } from 'express';
+import { createNote, deleteNote, getAllNotes, getNoteById, updateNote } from '../controllers/notesController.js';
+import { celebrate } from 'celebrate';
+import { createNoteSchema, getAllNotesSchema, noteIdSchema, updateNoteSchema } from '../validations/notesValidation.js';
+import { authenticate } from '../middleware/authenticate.js';
 
-export const getAllNotes = async (req, res) => {
-  const { page = 1, perPage = 10, tag, search } = req.query;
+const router = Router();
 
-  const skip = (page - 1) * perPage;
+router.use('/notes', authenticate);
 
-  const notesQuery = Note.find();
+router.get('/notes', celebrate(getAllNotesSchema), getAllNotes);
+router.get('/notes/:noteId', celebrate(noteIdSchema), getNoteById);
+router.post('/notes', celebrate(createNoteSchema), createNote);
+router.delete('/notes/:noteId', celebrate(noteIdSchema), deleteNote);
+router.patch('/notes/:noteId', celebrate(updateNoteSchema), updateNote);
 
-  if (search) {
-    notesQuery.where({
-      $text: { $search: search }
-    });
-  }
-  if (tag) {
-    notesQuery.where("tag").equals(tag);
-  }
-
-  const [totalNotes, notes] = await Promise.all([
-    notesQuery.clone().countDocuments(),
-    notesQuery.skip(skip).limit(perPage),
-  ]);
-
-   const totalPages = Math.ceil(totalNotes / perPage);
-
-  res.status(200).json({ page, perPage, totalNotes, totalPages, notes });
-};
-
-export const getNoteById = async (req, res, next) => {
-  const { noteId } = req.params;
-  const note = await Note.findById(noteId);
-
-  if (!note) {
-    next(createHttpError(404, 'Note not found'));
-    return;
-  }
-
-  res.status(200).json(note);
-};
-
-export const createNote = async (req, res) => {
-  const note = await Note.create(req.body);
-  res.status(201).json(note);
-};
-
-export const deleteNote = async (req, res, next) => {
-  const { noteId } = req.params;
-  const note = await Note.findOneAndDelete({
-    _id: noteId,
-  });
-
-  if (!note) {
-    next(createHttpError(404, "Note not found"));
-    return;
-  }
-
-  res.status(200).json(note);
-};
-
-
-export const updateNote = async (req, res, next) => {
-  const { noteId } = req.params;
-
-  const note = await Note.findOneAndUpdate(
-    { _id: noteId },
-    req.body,
-    { new: true },
-  );
-
-  if (!note) {
-    next(createHttpError(404, 'Note not found'));
-    return;
-  }
-
-  res.status(200).json(note);
-};
+export default router;
--- src/server.js
+++ src/server.js
@@ -1,29 +1,35 @@
+import 'dotenv/config';
 import express from 'express';
 import cors from 'cors';
-import 'dotenv/config';
+import helmet from 'helmet';
+import { errors } from 'celebrate';
+import cookieParser from 'cookie-parser';
+
 import { connectMongoDB } from './db/connectMongoDB.js';
+
 import { logger } from './middleware/logger.js';
 import { notFoundHandler } from './middleware/notFoundHandler.js';
 import { errorHandler } from './middleware/errorHandler.js';
+
+import authRoutes from './routes/authRoutes.js';
 import notesRoutes from './routes/notesRoutes.js';
-import { errors } from 'celebrate';
 
 const app = express();
-
 const PORT = process.env.PORT ?? 3000;
 
-app.use(logger);
 app.use(express.json());
-app.use(cors({ origin: "*" }));
+app.use(cors());
+app.use(cookieParser());
+app.use(helmet());
+app.use(logger);
 
+app.use(authRoutes);
 app.use(notesRoutes);
 
-app.get('/test-error', (req, res) => {
-  throw new Error('Simulated server error');
-});
-
 app.use(notFoundHandler);
+
 app.use(errors());
+
 app.use(errorHandler);
 
 await connectMongoDB();
